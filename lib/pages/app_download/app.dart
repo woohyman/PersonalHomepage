dbec0c2231ed9eb08cd2b14c0edceebb52354c6d
吴剑舞 <46742928+woohyman@users.noreply.github.com>
2023-10-24 16:06:10 +0800

    【增加】增加下载安装包页面

--- lib/pages/app_download/app.dart
+++ lib/pages/app_download/app.dart
@@ -1,12 +1,33 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:html' as html;
+
+import 'package:flutter/material.dart';
 
 class AppDownload extends StatelessWidget {
   const AppDownload({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("开发中。。。"),
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '点击右下角，下载最新的电脑端可执行文件.',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          const String url =
+              "https://taobaotool-1319006418.cos.ap-nanjing.myqcloud.com/setupfile/window/%E5%BA%94%E7%94%A8%E7%99%BE%E5%AE%9D%E7%AE%B1.zip";
+          html.AnchorElement anchorElement = html.AnchorElement(href: url);
+          anchorElement.download = url;
+          anchorElement.click();
+        },
+        child: const Icon(Icons.download),
+      ),
     );
   }
 }
